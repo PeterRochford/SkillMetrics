c3efb7c3699ab54b86bf996fcf1d1074f484982d
Peter A. Rochford <prochford@acornsi.com>
2016-12-27 14:01:43 -0500

    Replaced axis specification of 'square' to 'equal' to fix matplotlib bug.

--- skill_metrics/plot_target_axes.py
+++ skill_metrics/plot_target_axes.py
@@ -20,39 +20,39 @@ def plot_target_axes(axes):
     ytick = axes['ytick']
     
     # Draw axis lines
-    plt.plot([xtick[0], xtick[-1]], [0, 0],'k') # x-axis
+    plt.plot([xtick[0], xtick[-1]], [0, 0], 'k')  # x-axis
     plt.hold(True)
-    plt.plot([0, 0],[ytick[0], ytick[-1]],'k') # y-axis
+    plt.plot([0, 0], [ytick[0], ytick[-1]], 'k')  # y-axis
     
     # Get offsets
     gca = plt.gca()
-    Xoff = np.diff(gca.get_xlim())[0]/40
-    Yoff = np.diff(gca.get_ylim())[0]/40
+    Xoff = np.diff(gca.get_xlim())[0] / 40
+    Yoff = np.diff(gca.get_ylim())[0] / 40
     
     # Plot new ticks with labels
     y = -2.*Yoff
     for i, x in enumerate(xtick):
-        plt.plot([x, x],[0, Yoff],'-k')
-        plt.text(x,y,axes['xlabel'][i],horizontalalignment='center')
+        plt.plot([x, x], [0, Yoff], '-k')
+        plt.text(x, y, axes['xlabel'][i], horizontalalignment='center')
 
     x = -1.*Xoff
     for i, y in enumerate(ytick):
         if i > 0:
-            plt.plot([Xoff, 0],[y, y],'-k')
+            plt.plot([Xoff, 0], [y, y], '-k')
         else:
-            plt.plot([Xoff, 0],[y-0.01*Yoff, y-0.01*Yoff],'-k')
+            plt.plot([Xoff, 0], [y - 0.01 * Yoff, y - 0.01 * Yoff], '-k')
             
-        plt.text(x,y-0.5*Yoff,axes['ylabel'][i],horizontalalignment='right')
+        plt.text(x, y - 0.5 * Yoff, axes['ylabel'][i], horizontalalignment='right')
 
     # Label x-axis with text at end of axis
-    xpos = xtick[-1] + 3*xtick[-1]/30;  ypos = 0
-    plt.text(xpos,ypos,'uRMSD',color='k',horizontalalignment='left')
+    xpos = xtick[-1] + 3 * xtick[-1] / 30;  ypos = 0
+    plt.text(xpos, ypos, 'uRMSD', color='k', horizontalalignment='left')
     
     # Label y-axis with text at end of axis
-    xpos = 0;  ypos = ytick[-1] + 3*ytick[-1]/30
-    plt.text(xpos,ypos,'Bias',color='k',horizontalalignment='center')
+    xpos = 0;  ypos = ytick[-1] + 3 * ytick[-1] / 30
+    plt.text(xpos, ypos, 'Bias', color='k', horizontalalignment='center')
     
-    plt.setp(gca,frame_on=False)
-    plt.axis('square')
+    plt.setp(gca, frame_on=False)
+    plt.axis('equal')
     plt.axis('off')
     plt.gcf().patch.set_facecolor('w')
