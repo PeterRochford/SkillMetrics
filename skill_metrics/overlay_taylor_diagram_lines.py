b92393d56b64a199b217b5fd96c183d328be8a21
Peter Rochford <prochford@thesymplectic.com>
2019-01-05 13:12:56 -0500

    Improved annotation of correlation coefficient axis

--- skill_metrics/overlay_taylor_diagram_lines.py
+++ skill_metrics/overlay_taylor_diagram_lines.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 
 def overlay_taylor_diagram_lines(axes,cax,option):
@@ -14,6 +15,7 @@ def overlay_taylor_diagram_lines(axes,cax,option):
     option : data structure containing option values. (Refer to 
              GET_TAYLOR_DIAGRAM_OPTIONS function for more information.)
     option['colcor']   : CORs grid and tick labels color (Default: blue)
+    option['numberpanels'] : number of panels
     option['showlabelscor'] : Show or not the CORRELATION tick labels
     option['stylecor'] : Linestyle of the CORs grid
     option['tickcor']  : CORs values to plot lines from origin
@@ -31,8 +33,7 @@ def overlay_taylor_diagram_lines(axes,cax,option):
    '''
 
     # DRAW CORRELATION LINES EMANATING FROM THE ORIGIN:
-    npanels = option['numberpanels']
-    corr = option['tickcor'][npanels-1]
+    corr = option['tickcor'][option['numberpanels']-1]
     th  = np.arccos(corr)
     cst = np.cos(th); snt = np.sin(th);
     cs = np.append(-1.0*cst, cst)
@@ -42,9 +43,16 @@ def overlay_taylor_diagram_lines(axes,cax,option):
                  linestyle = option['stylecor'],
                  color = option['colcor'], linewidth = option['widthcor'])
     
-    # annotate the lines by correlation coefficient
+    # annotate them in correlation coefficient
     if option['showlabelscor'] == 'on':
+        fontSize = matplotlib.rcParams.get('font.size')
         rt = 1.05*axes['rmax']
         for i,cc in enumerate(corr):
-            plt.text(rt*cst[i],rt*snt[i],str(round(cc,2)), 
-                     horizontalalignment = 'center', color = option['colcor'])
+            if option['numberpanels'] == 2:
+                x = (1.05+abs(cst[i])/30)*axes['rmax']*cst[i]
+            else:
+                x = rt*cst[i]
+            y = rt*snt[i]
+            plt.text(x,y,str(round(cc,2)),
+                     horizontalalignment = 'center',
+                     color = option['colcor'], fontsize = fontSize)
