a4f67a5d76634437cdea7df1520aa605431def91
Peter Rochford <prochford@thesymplectic.com>
2022-06-25 12:39:45 -0400

    Documented this library file.

--- skill_metrics/utils.py
+++ skill_metrics/utils.py
@@ -1,9 +1,27 @@
 import numpy as np
 
+'''
+Library of utilities to support the SkillMetrics package
+'''
 
-def check_arrays(predicted,
-                 reference):
-    """Generic check of input arrays."""
+def check_arrays(predicted, reference):
+    '''
+    Checks two arrays have the same dimensions.
+
+    Input:
+    PREDICTED : predicted field
+    REFERENCE : reference field
+
+    Author: Tim Cera
+        St. Johns River Water Management District
+        University of Florida
+        Gainesville, FL
+        https://www.linkedin.com/in/timcera
+        https://github.com/timcera
+
+    Created on Mar 23, 2019
+    '''
+
     pdims = predicted.shape
     rdims = reference.shape
     if not np.array_equal(pdims, rdims):
