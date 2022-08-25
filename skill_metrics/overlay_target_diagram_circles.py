9a945535daaa873acfeec29dde00bed77f27e1dd
Peter Rochford <prochford@thesymplectic.com>
2022-08-25 17:35:42 -0400

    Circles are now drawn on subplot axes.

--- skill_metrics/overlay_target_diagram_circles.py
+++ skill_metrics/overlay_target_diagram_circles.py
@@ -1,7 +1,26 @@
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 
-def overlay_target_diagram_circles(option):
+def pol2cart(phi, rho):
+    '''
+    Transforms corresponding elements of polar coordinate arrays to 
+    Cartesian coordinates.
+    
+    INPUTS:
+    phi : polar angle counter-clockwise from x-axis in radians
+    rho : radius
+    
+    OUTPUTS:
+    x   : Cartesian x-coordinate
+    y   : Cartesian y-coordinate
+    '''
+
+    x = np.multiply(rho, np.cos(phi))
+    y = np.multiply(rho, np.sin(phi))
+    return x, y
+
+def overlay_target_diagram_circles(ax: matplotlib.axes.Axes, option: dict) -> None:
     '''
     Overlays circle contours on a target diagram.
     
@@ -9,8 +28,8 @@ def overlay_target_diagram_circles(option):
     deviation ranges and observational uncertainty threshold.
     
     INPUTS:
-    option : dictionary containing option values. (Refer to 
-             GET_TARGET_DIAGRAM_OPTIONS function for more information.)
+    ax     : matplotlib.axes.Axes object in which the Taylor diagram will be
+             plotted
     option['axismax'] : maximum for the X & Y values. Used to set
             default circles when no contours specified
     option['circles'] : radii of circles to draw to indicate isopleths 
@@ -36,46 +55,30 @@ def overlay_target_diagram_circles(option):
     if option['normalized'] == 'on':
         rho = unit
         X, Y = pol2cart(theta, rho)
-        plt.plot(X, Y, 'k', 'LineWidth', option['circleLineWidth'])
+        ax.plot(X, Y, 'k', 'LineWidth', option['circleLineWidth'])
 
     # Set range for target circles
     if option['normalized'] == 'on':
         circles = [.5, 1]
     else:
-        if len(option['circles']) > 0:
+        if option['circles'] is None:
+            circles = [option['axismax'] * x for x in [.7, 1]]
+        else:
             circles = np.asarray(option['circles'])
             index = np.where(circles <= option['axismax'])
             circles = [option['circles'][i] for i in index[0]]
-        else:
-            circles = [option['axismax'] * x for x in [.7, 1]]
-
+    
     # 2 - secondary circles
     for c in circles:
         rho = c * unit
         X, Y = pol2cart(theta, rho)
-        plt.plot(X, Y, option['circlelinespec'],
-                 linewidth=option['circlelinewidth'])
+        ax.plot(X, Y, linestyle=option['circlestyle'],
+                color=option['circlecolor'],
+                linewidth=option['circlelinewidth'])
+    del c
 
     # 3 - Observational Uncertainty threshold
     if option['obsuncertainty'] > 0:
         rho = option['obsuncertainty'] * unit
         X, Y = pol2cart(theta, rho)
-        plt.plot(X, Y, '--b')
-
-def pol2cart(phi, rho):
-    '''
-    Transforms corresponding elements of polar coordinate arrays to 
-    Cartesian coordinates.
-    
-    INPUTS:
-    phi : polar angle counter-clockwise from x-axis in radians
-    rho : radius
-    
-    OUTPUTS:
-    x   : Cartesian x-coordinate
-    y   : Cartesian y-coordinate
-    '''
-
-    x = np.multiply(rho, np.cos(phi))
-    y = np.multiply(rho, np.sin(phi))
-    return x, y
+        ax.plot(X, Y, '--b')
