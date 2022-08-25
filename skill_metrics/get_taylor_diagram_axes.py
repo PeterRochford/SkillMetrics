ef771dc33bed25c2108e72a394f619d25f5cd5be
Peter Rochford <prochford@thesymplectic.com>
2022-08-24 22:44:39 -0400

    Axes are now drawn on subplot axes.

--- skill_metrics/get_taylor_diagram_axes.py
+++ skill_metrics/get_taylor_diagram_axes.py
@@ -2,18 +2,19 @@ import matplotlib.ticker as ticker
 import matplotlib.pyplot as plt
 import numpy as np
 
-def get_taylor_diagram_axes(rho,option):
+def get_taylor_diagram_axes(ax, rho, option) -> dict:
     '''
     Get axes value for taylor_diagram function.
     
     Determines the axes information for a Taylor diagram given the axis 
     values (X,Y) and the options in the dictionary OPTION returned by 
     the GET_TAYLOR_DIAGRAM_OPTIONS function.
- 
+
     INPUTS:
+    ax     : the matplotlib.axes.Axes to receive the plot
     rho    : radial coordinate
     option : dictionary containing option values. (Refer to 
-             GET_TAYLOR_DIAGRAM_OPTIONS function for more information.)
+             get_taylor_diagram_subplot_options() function for more information.)
  
     OUTPUTS:
     axes         : dictionary containing axes information for Taylor diagram
@@ -23,35 +24,35 @@ def get_taylor_diagram_axes(rho,option):
     axes['rmax'] : maximum value for radial coordinate
     axes['rmin'] : minimum value for radial coordinate
     axes['tc']   : color for x-axis
-    option       : dictionary containing updated option values
-  
-    Author: Peter A. Rochford
-        Symplectic, LLC
-        www.thesymplectic.com
-        prochford@thesymplectic.com
+    Also modifies the input variables 'ax' and 'option'
+    
+    Authors: Peter A. Rochford
+        rochford.peter1@gmail.com
 
-    Created on Dec 3, 2016
-    Revised on Dec 30, 2018
+    Andre D. L. Zanchetta (adapting Peter A. Rochford's code)
+        adlzanchetta@gmail.com
+
+    Created on Nov 25, 2016
+    Revised on Aug 14, 2022
     '''
 
     axes = {}
     axes['dx'] = rho[0]
        
-    cax = plt.gca()
-    axes['tc'] = cax.xaxis.label.get_color()
+    axes['tc'] = option['colframe']
     axes['next'] = 'replace' #needed?
     
     # make a radial grid
     if option['axismax'] == 0.0:
         maxrho = max(abs(rho))
     else:
-        maxrho = option['axismax'];
+        maxrho = option['axismax']
 
     # Determine default number of tick marks
     if option['overlay'] =='off':
-        plt.xlim(-maxrho,maxrho)
-    xt, lab = plt.xticks()
-    ticks = sum(xt >= 0);
+        ax.set_xlim(-maxrho, maxrho)
+    xt = ax.get_xticks()
+    ticks = sum(xt >= 0)
     
     # Check radial limits and ticks
     axes['rmin'] = 0; 
@@ -67,11 +68,15 @@ def get_taylor_diagram_axes(rho,option):
         elif rticks % 3 == 0:
             rticks = rticks/3
     axes['rinc']  = (axes['rmax'] - axes['rmin'])/rticks
-    tick  = np.arange(axes['rmin'] + axes['rinc'],axes['rmax'] + axes['rinc'],axes['rinc']) 
+    tick  = np.arange(axes['rmin'] + axes['rinc'],
+                      axes['rmax'] + axes['rinc'],
+                      axes['rinc']) 
     
     if len(option['tickrms']) == 0:
-        option['tickrms'] = tick; option['rincrms'] = axes['rinc']
+        option['tickrms'] = tick
+        option['rincrms'] = axes['rinc']
     if len(option['tickstd']) == 0:
-        option['tickstd'] = tick; option['rincstd'] = axes['rinc']
+        option['tickstd'] = tick
+        option['rincstd'] = axes['rinc']
     
-    return axes, cax
+    return axes
